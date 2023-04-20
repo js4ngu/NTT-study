5b9583aec31c7901ed12d6f01227f1d31f3eedea
Jongsang yoo <jamin0513@gmail.com>
2023-04-20 15:38:30 +0900

    navieNTT modelsim complete!

--- SW/ntt_ref.c
+++ SW/ntt_ref.c
@@ -11,8 +11,8 @@ int main() {
 
 void naiveNTT() {
     int mod = 17;
-    int root = 15;
-    int dataIN[N] = {3, 0, 1, 2, 0, 0, 0, 0};
+    int root = 9;
+    int dataIN[N] = {3, 1, 2, 6, 0, 0, 0, 0};
     int factor;
     int temp;
     int dataOUT[N];
@@ -21,16 +21,19 @@ void naiveNTT() {
         temp = 0;
         for (int j = 0; j < N; j++) {
             factor = 1;
+            // pow
             for (int k = 0; k < i * j; k++) {
                 factor = (factor * root) % mod;
             }
+            
             temp = (temp + (dataIN[j] * factor)) % mod;
         }
         dataOUT[i] = temp;
     }
 
-    printf("6 8 11 12 2 6 10 3\n");
     for (int i = 0; i < N; i++)
         printf("%d ", dataOUT[i]);
     printf("\n");
 }
+
+//12, 9, 4, 12, 15, 15, 15, 10'
