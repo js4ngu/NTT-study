8c38d50f2dbda04c5058ae2e44c9fe48640f5b59
Jongsang yoo <jamin0513@gmail.com>
2023-05-11 22:03:04 +0900

    4point FNTT 검증완료

--- HW/naiveFNTT3/src/test/scala/FNTT/FNTT_4pointTB.scala
+++ HW/naiveFNTT3/src/test/scala/FNTT/FNTT_4pointTB.scala
@@ -10,23 +10,21 @@ class FNTT_4pointTB extends AnyFlatSpec with ChiselScalatestTester {
     test(new FNTT_4point(width = 8)).withAnnotations(Seq(WriteVcdAnnotation)) { p =>
       //HW Init
       p.io.omega(0).poke(1.U(8.W))
-      p.io.omega(1).poke(9.U(8.W))
-      p.io.omega(2).poke(17.U(8.W))
-      p.io.omega(3).poke(9.U(8.W))
+      p.io.omega(2).poke(16.U(8.W))
 
       p.io.mod.poke(17.U(8.W))
 
       //Data Injecton
       p.io.inData(0).poke(1.U(8.W))
-      p.io.inData(1).poke(0.U(8.W))
-      p.io.inData(2).poke(1.U(8.W))
+      p.io.inData(1).poke(1.U(8.W))
+      p.io.inData(2).poke(0.U(8.W))
       p.io.inData(3).poke(0.U(8.W))
 
-      p.clock.step(10)
+      p.clock.step(20)
 
       p.io.outData(0).expect(2.U(8.W))
-      p.io.outData(1).expect(1.U(8.W))
-      p.io.outData(2).expect(1.U(8.W))
+      p.io.outData(1).expect(0.U(8.W))
+      p.io.outData(2).expect(2.U(8.W))
       p.io.outData(3).expect(0.U(8.W))
     }
   }
