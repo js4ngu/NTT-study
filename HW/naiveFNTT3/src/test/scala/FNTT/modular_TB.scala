18454c93ee3c055cb9b135d8b5bde66668b29a6d
Jongsang yoo <jamin0513@gmail.com>
2023-05-11 20:06:10 +0900

    2point NTT까진 OK

--- HW/naiveFNTT3/src/test/scala/FNTT/modular_TB.scala
+++ HW/naiveFNTT3/src/test/scala/FNTT/modular_TB.scala
@@ -6,6 +6,14 @@ import FNTT.modular
 
 import org.scalatest.flatspec.AnyFlatSpec
 
-class modulat_TB {
-
+class modularTB extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Modular"
+  it should "Mod ex" in {
+    test(new modular(width = 8)).withAnnotations(Seq(WriteVcdAnnotation)) { p =>
+      p.io.inData.poke(5.U(8.W))
+      p.io.mod.poke(3.U(8.W))
+      p.clock.step(10)
+      p.io.outData.expect(2.U(8.W))
+    }
+  }
 }
